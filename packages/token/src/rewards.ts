0e99f599d3ddac41a0eb96322c6d8ae6640bff9a
Chris Huang-Leaver <176128+zeonglow@users.noreply.github.com>
2025-10-09 15:07:49 +1100

    21 create an api for issuing rewards to a given wallet address (#33)
    * feature: placeholder API call
    
    * fix: return after error found
    
    * update:  expose program API
    
    * chore: run prettier
    
    * feature: Use simple rewarder app
    
    * feature: link api to rewarder
    
    * fix: Remove debugging console log

--- packages/token/src/rewards.ts
+++ packages/token/src/rewards.ts
@@ -1,3 +1,65 @@
+import { ethers, Wallet } from "ethers";
+import ERC20_ABI from "./erc20abi.json" with { type: "json" };
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export interface IRewarderOptions {
+  rpcUrl: string;
+  masterAddress: string;
+  walletKey: string;
+  tokenContractAddress: string;
+}
+
+class Rewarder {
+  constructor(options?: IRewarderOptions) {
+    const {
+      rpcUrl = process.env.SEPOLIA_RPC_URL ?? "",
+      walletKey = process.env.PRIVATE_KEY ?? "",
+      masterAddress = process.env.KUDOS_MASTER_WALLET ?? "",
+      tokenContractAddress = process.env.KUDOS_TOKEN_CONTRACT_ADDRESS ?? "",
+    } = options ?? {};
+    this.rpcUrl = rpcUrl;
+    this.provider = new ethers.JsonRpcProvider(rpcUrl);
+    this.wallet = new Wallet(walletKey, this.provider);
+    this.masterAddress = masterAddress;
+    this.tokenContractAddress = tokenContractAddress;
+  }
+
+  private readonly rpcUrl: string;
+  private readonly provider: ethers.JsonRpcProvider;
+  private readonly wallet: ethers.Wallet;
+  private readonly masterAddress: string;
+  private readonly tokenContractAddress: string;
+
+  async getCurrentBlock() {
+    return await this.provider.getBlockNumber();
+  }
+
+  async getMasterAccountBalance(): Promise<bigint> {
+    return await this.provider.getBalance(this.masterAddress);
+  }
+
+  async sendReward(to: string, amount: bigint, confirm: boolean = true) {
+    const tokenContract = new ethers.Contract(
+      this.tokenContractAddress,
+      ERC20_ABI,
+      this.wallet,
+    );
+
+    const transaction = await tokenContract.transfer(to, amount);
+
+    const receipt = confirm ? await transaction.wait() : undefined;
+
+    return {
+      transaction,
+      receipt,
+    };
+  }
+}
+
+const rewarder = new Rewarder();
+
 export async function giveReward(args: { to: string; amount: bigint }) {
-  return false;
+  const { to, amount } = args;
+  return await rewarder.sendReward(to, amount, false);
 }
